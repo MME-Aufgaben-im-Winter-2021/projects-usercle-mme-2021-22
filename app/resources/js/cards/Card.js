c70970989f04e82c73b78028861fd6d23169a335
yannicmiedaner <yannicmiedaner@gmail.com>
2022-03-09 17:18:30 +0100

    index.js, card.js, observer.js, style.css

---
+++ app/resources/js/cards/Card.js
@@ -0,0 +1,25 @@
+import { Observable, Event } from "../utils/Observer.js";
+
+class Card {
+
+    constructor(title, text){
+        this.title = title;
+        this.text = text;
+    }
+
+    addCard() {
+        console.log("### addCard ausgeführt ###");
+
+        // Hier klonen und Einfügen
+        let temp = document.getElementById("box-template");
+        let clone = temp.content.cloneNode(true);
+
+        clone.querySelector(".header h2").innerHTML = this.title;
+        clone.querySelector(".container p").innerHTML = this.text;
+
+        document.querySelector(".cards").appendChild(clone);
+    }
+
+}
+
+export default Card;
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,18 @@
+import Card from "../resources/js/cards/Card.js";
+
 function init() {
     console.log("### Starting MME Project ###");
+    let addButton = document.querySelector(".button-new-card");
+    addButton.addEventListener("click", function(){
+        console.log("add new card");
+        createNewCard();
+    })
+}
+
+function createNewCard() {
+    var myCard = new Card("Beispielitel", "Beispieltext");
+    console.log("My Card has title: " + myCard.title + " and text: " + myCard.text);
+    myCard.addCard();
 }
 
 init();
