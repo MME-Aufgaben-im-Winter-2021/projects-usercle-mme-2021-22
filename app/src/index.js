9dbc9598e9aada2c92825d3cb8db581db2153e6f
Alexander Bazo <alexanderbazo@googlemail.com>
2022-03-11 16:05:07 +0100

    Refactoring

--- app/src/index.js
+++ app/src/index.js
@@ -1,48 +1,48 @@
-import Card from "../resources/js/cards/Card.js";
-import { turnCard, reverseCard } from "../resources/js/cards/Card.js";
+import Card from "./cards/Card.js";
+import { turnCard, reverseCard } from "./cards/Card.js";
 
 var newCard = new Card("Title", "Beispieltext");
 
 function init() {
-    initUI();
+  initUI();
 }
 
 function initUI() {
-    let addButton = document.querySelector(".button-new-card"),
-        editTitleButton = document.querySelector(".header h2"),
-        editTextButton = document.querySelector(".container p"),
-        turnCardButton = document.querySelectorAll(".turn"),
-        reverseCardButton = document.querySelectorAll(".reverse");
+  let addButton = document.querySelector(".button-new-card"),
+    editTitleButton = document.querySelector(".header h2"),
+    editTextButton = document.querySelector(".container p"),
+    turnCardButton = document.querySelectorAll(".turn"),
+    reverseCardButton = document.querySelectorAll(".reverse");
 
-    addButton.addEventListener("click", createNewCard);
-    editTitleButton.addEventListener("click", editTitle);
-    editTextButton.addEventListener("click", editText);
+  addButton.addEventListener("click", createNewCard);
+  editTitleButton.addEventListener("click", editTitle);
+  editTextButton.addEventListener("click", editText);
 
-    turnCardButton.forEach(function (turn, i) {
-        turn.addEventListener("click", function handleClick() {
-            let innerCard = turnCardButton[i].parentNode.parentNode;
-            turnCard(innerCard);
-        });
+  turnCardButton.forEach(function(turn, i) {
+    turn.addEventListener("click", function handleClick() {
+      let innerCard = turnCardButton[i].parentNode.parentNode;
+      turnCard(innerCard);
     });
+  });
 
-    reverseCardButton.forEach(function (reverse, i) {
-        reverse.addEventListener("click", function handleClick() {
-            let innerCard = reverseCardButton[i].parentNode.parentNode;
-            reverseCard(innerCard);
-        });
+  reverseCardButton.forEach(function(reverse, i) {
+    reverse.addEventListener("click", function handleClick() {
+      let innerCard = reverseCardButton[i].parentNode.parentNode;
+      reverseCard(innerCard);
     });
+  });
 }
 
 function createNewCard() {
-    newCard.addCard();    
+  newCard.addCard();
 }
 
 function editTitle() {
-    newCard.editTitle();
+  newCard.editTitle();
 }
 
 function editText() {
-    newCard.editText();
+  newCard.editText();
 }
 
 init();
