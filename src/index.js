3164f9b919940bd115febafb2b366061deafd7a8
alexanderbazo <alexanderbazo@googlemail.com>
2022-03-11 15:12:43 +0000

    Deploying to gh-pages from @ MME-Aufgaben-im-Winter-2021/projects-usercle-mme-2021-22@3c31aa5f669ed7199dc4bfbd8429aace424a9ea1 🚀

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
+import Card from "./cards/Card.js";
+import { turnCard, reverseCard } from "./cards/Card.js";
+
+var newCard = new Card("Title", "Beispieltext");
+
 function init() {
-    console.log("### Starting MME Project ###");
+  initUI();
+}
+
+function initUI() {
+  let addButton = document.querySelector(".button-new-card"),
+    editTitleButton = document.querySelector(".header h2"),
+    editTextButton = document.querySelector(".container p"),
+    turnCardButton = document.querySelectorAll(".turn"),
+    reverseCardButton = document.querySelectorAll(".reverse");
+
+  addButton.addEventListener("click", createNewCard);
+  editTitleButton.addEventListener("click", editTitle);
+  editTextButton.addEventListener("click", editText);
+
+  turnCardButton.forEach(function(turn, i) {
+    turn.addEventListener("click", function handleClick() {
+      let innerCard = turnCardButton[i].parentNode.parentNode;
+      turnCard(innerCard);
+    });
+  });
+
+  reverseCardButton.forEach(function(reverse, i) {
+    reverse.addEventListener("click", function handleClick() {
+      let innerCard = reverseCardButton[i].parentNode.parentNode;
+      reverseCard(innerCard);
+    });
+  });
+}
+
+function createNewCard() {
+  newCard.addCard();
+}
+
+function editTitle() {
+  newCard.editTitle();
+}
+
+function editText() {
+  newCard.editText();
 }
 
 init();
